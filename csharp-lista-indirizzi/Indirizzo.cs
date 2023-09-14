e76c75b3241632c66e5c4a680a77ff6de6c3ad51
Marco Rinaldi <marcorinaldi98@libero.it>
2023-09-14 13:06:03 +0200

    istanziata la classe Indirizzo

---
+++ csharp-lista-indirizzi/Indirizzo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public class Indirizzo
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string Street { get; private set; }
+        public string City { get; private set; }
+        public string Province { get; private set; }
+        public int ZIP { get; private set; }
+
+        public Indirizzo(string name, string surname, string street, string city, string province, int zip)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Street = street;
+            this.City = city;
+            this.Province = province;
+            this.ZIP = zip;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.Surname} abita a {this.Street}, {this.City}, {this.Province}, {this.ZIP}";
+        }
+    }
+}
