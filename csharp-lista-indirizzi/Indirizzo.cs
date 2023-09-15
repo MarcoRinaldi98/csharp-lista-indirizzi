0064145f3d76b011167cd5fbeee214e4d813e893
Marco Rinaldi <marcorinaldi98@libero.it>
2023-09-15 10:23:16 +0200

    perfezionamento codice e gestione casi limite

--- csharp-lista-indirizzi/Indirizzo.cs
+++ csharp-lista-indirizzi/Indirizzo.cs
@@ -8,6 +8,7 @@ namespace csharp_lista_indirizzi
 {
     public class Indirizzo
     {
+        // ATTRIBUTI
         public string Name { get; private set; }
         public string Surname { get; private set; }
         public string Street { get; private set; }
@@ -15,6 +16,7 @@ namespace csharp_lista_indirizzi
         public string Province { get; private set; }
         public int ZIP { get; private set; }
 
+        // COSTRUTTORE
         public Indirizzo(string name, string surname, string street, string city, string province, int zip)
         {
             this.Name = name;
@@ -25,9 +27,56 @@ namespace csharp_lista_indirizzi
             this.ZIP = zip;
         }
 
+        // METODI
         public override string ToString()
         {
             return $"{this.Name} {this.Surname} abita a {this.Street}, {this.City}, {this.Province}, {this.ZIP};";
         }
+
+        // METODI STATIC
+        public static Indirizzo ParseFromLine(string line)
+        {
+
+            string[] stringSplits = line.Split(',');
+
+            if (stringSplits.Length != 6)
+            {
+                throw new Exception($"L'indirizzo ({line}) non è leggibile!");
+            }
+            else
+            {
+                //definisco una variabile che ad ogni elemento valido aumenterà
+                int counter = 0;
+
+                for (int i = 0; i < stringSplits.Length; i++)
+                {
+                    stringSplits[i] = stringSplits[i].Trim();
+                    if (stringSplits[i].Length > 0)
+                    {
+                        counter++;
+                    }
+                }
+
+                if (counter == 6)
+                {
+                    string nome = stringSplits[0];
+                    string cognome = stringSplits[1];
+                    string via = stringSplits[2];
+                    string citta = stringSplits[3];
+                    string provincia = stringSplits[4];
+                    int zip = int.Parse(stringSplits[5]);
+
+                    Console.WriteLine($"Indirizzo di {nome} {cognome}: {via}, {citta}, {provincia}, {zip};");
+
+                    Indirizzo nuovoIndirizzo = new Indirizzo(nome, cognome, via, citta, provincia, zip);
+                    return nuovoIndirizzo;
+                }
+                else
+                {
+                    throw new Exception($"L'indirizzo ({line}) contiene valori non validi!");
+                }
+            }
+        }
+
     }
 }
--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -6,6 +6,7 @@ try
 {
     // leggo il file che si trova all'indirizzo indicato
     StreamReader fileIndirizzi = File.OpenText("C:\\Users\\Marco\\source\\repos\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\my-addresses.csv");
+    
     // variabile per la gestione di ogni riga nel file
     int lineNumber = 0;
 
@@ -17,49 +18,18 @@ try
             // leggo riga per riga il file
             string line = fileIndirizzi.ReadLine();
             lineNumber++;
+
             // inizio a registrare i dati dalla seconda riga
             if (lineNumber > 1)
             {
-                string[] stringSplits = line.Split(',');
-
-                if (stringSplits.Length != 6)
+                try
                 {
-                    Console.WriteLine($"L'indirizzo ({line}) non è leggibile!");
+                    Indirizzo addressReaded = Indirizzo.ParseFromLine(line);
+                    listaIndirizzi.Add(addressReaded);
                 }
-                else
+                catch (Exception ex)
                 {
-                    //definisco una variabile che ad ogni elemento valido aumenterà
-                    int counter = 1;
-                    // definisco una variabile booleana che se il counter arrivera a 6 indicherà che la linea letta è valida all'inserimento dati
-                    bool lineaValida = false;
-                    foreach (var element in stringSplits)
-                    {
-                        if (element.Length > 0)
-                        {
-                            counter++;
-                        }
-                        if(counter == 6)
-                        {
-                            lineaValida = true;
-                        }
-                    }
-                    if (lineaValida)
-                    {
-                        string nome = stringSplits[0];
-                        string cognome = stringSplits[1];
-                        string via = stringSplits[2];
-                        string citta = stringSplits[3];
-                        string provincia = stringSplits[4];
-                        int zip = int.Parse(stringSplits[5]);
-
-                        Console.WriteLine($"Indirizzo di {nome} {cognome}: {via}, {citta}, {provincia}, {zip};");
-
-                        Indirizzo nuovoIndirizzo = new Indirizzo(nome, cognome, via, citta, provincia, zip);
-                        listaIndirizzi.Add(nuovoIndirizzo);
-                    } else
-                    {
-                        Console.WriteLine($"L'indirizzo ({line}) contiene valori non validi!");
-                    }
+                    Console.WriteLine(ex.Message);
                 }
             }
         }
@@ -70,8 +40,7 @@ try
     {
         fileIndirizzi.Close();
     }
-    // ogni volta bisogna chiudere il file dopo l'utilizzo
-    fileIndirizzi.Close();
+  
 } catch(Exception e)
 {
     Console.WriteLine(e.Message);
